72ba35b297ac4e3539b3ec23e7545debd0859073
Ravi Thapa <meroravi0@gmail.com>
2022-01-13 20:44:41 +0545

    add random data

---
+++ android/app/src/main/kotlin/com/example/onnfood/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.onnfood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final double price;
+  const ProductItem(
+      {Key? key,
+      required this.id,
+      required this.title,
+      required this.imageUrl,
+      required this.price})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Stack(
+          children: [
+            SizedBox(height: 100, width: 100, child: Image.network(imageUrl)),
+            const Padding(
+              padding: EdgeInsets.only(left: 50),
+              child: Text('40% off'),
+            )
+          ],
+        ),
+        Text(title),
+        Text(
+          price.toString(),
+          style: const TextStyle(decoration: TextDecoration.lineThrough),
+        ),
+      ],
+    );
+  }
+}
